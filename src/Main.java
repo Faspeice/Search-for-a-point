0b1397e2bde4d720d61735748697caf27006e110
Faspeice <154542847+Faspeice@users.noreply.github.com>
2024-10-05 14:38:46 +0600

    Add min finder func

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 import java.util.Random;
 public class Main {
-    public static double calculate_distance(int[] first_point, int[] second_point)
+    public static double calculateDistance(int[] firstPoint, int[] secondPoint)
     {
-        double distance = Math.sqrt(Math.pow(first_point[0] - second_point[0], 2) + Math.pow(first_point[1] - second_point[1], 2));
-        return distance;
+        return Math.sqrt(Math.pow(firstPoint[0] - secondPoint[0], 2) + Math.pow(firstPoint[1] - secondPoint[1], 2));
     };
     public static void main(String[] args) {
         Random rand = new Random();
-        int[][] points = new int[40][2];
-        for (int i = 1; i < 40; i++) {
-            points[i][0] = rand.nextInt(-100,100);
-            points[i][1] = rand.nextInt(-100,100);
-            System.out.println("Point " + (i+1) + " (" + points[i][0] +", " + points[i][1] + ")\n");
-            System.out.println(calculate_distance(points[i], points[i-1]));
+        int n = 40;
+        int[] minPoint = new int[2];
+        double minDistanceSum = Double.POSITIVE_INFINITY, distanceSum = 0;
+        int[][] points = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            points[i][0] = rand.nextInt(-10,10);
+            points[i][1] = rand.nextInt(-10,10);
         }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                distanceSum += calculateDistance(points[i], points[j]);
+
+        }
+            System.out.println("Point " + (i+1) + " (" + points[i][0] +", " + points[i][1] + ")");
+            System.out.println(distanceSum);
+            if(distanceSum<minDistanceSum){
+                minDistanceSum = distanceSum;
+                minPoint[0] = points[i][0];
+                minPoint[1] = points[i][1];
+            }
+            distanceSum = 0;
+        }
+        System.out.println("\n" + "(" + minPoint[0] +", " + minPoint[1] + ") " + minDistanceSum);
     }
 }
